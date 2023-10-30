70dc58822fb3d04424df82db736d7ed578d4d772
krystkowiakk@gmail.com <krystkowiakk@gmail.com>
2023-10-30 20:48:39 +0100

    load config

---
+++ utilities/config.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+AI_DEVS_SERVER = os.environ.get('AI_DEVS_SERVER')
+AI_DEVS_API_KEY = os.environ.get('AI_DEVS_API_KEY')
+OPEN_AI_API_KEY = os.environ.get('OPEN_AI_API_KEY')
