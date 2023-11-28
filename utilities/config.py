33ec0b50e1d5dc073da667a532907a6d6a0f5dbe
Krystkowiakk <krystkowiakk@gmail.com>
2023-11-28 17:35:11 -0300

    update

--- utilities/config.py
+++ utilities/config.py
@@ -7,5 +7,6 @@ load_dotenv(find_dotenv())
 AI_DEVS_SERVER = os.environ.get('AI_DEVS_SERVER')
 AI_DEVS_API_KEY = os.environ.get('AI_DEVS_API_KEY')
 OPEN_AI_API_KEY = os.environ.get('OPEN_AI_API_KEY')
+RENDER_FORM_API_KEY = os.environ.get('RENDER_FORM_API_KEY')
 
 qdrant_url = os.environ.get('QDRANT_URL')
