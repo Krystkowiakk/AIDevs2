2a4645e90be1989c0706b5dfe945b697998ba61f
Krystkowiakk <krystkowiakk@gmail.com>
2023-12-01 13:52:26 -0300

    update

--- utilities/config.py
+++ utilities/config.py
@@ -8,5 +8,6 @@ AI_DEVS_SERVER = os.environ.get('AI_DEVS_SERVER')
 AI_DEVS_API_KEY = os.environ.get('AI_DEVS_API_KEY')
 OPEN_AI_API_KEY = os.environ.get('OPEN_AI_API_KEY')
 RENDER_FORM_API_KEY = os.environ.get('RENDER_FORM_API_KEY')
+SERP_API_KEY = '36d8cd99f841a2b7c8aabaf9f7799fe7f32742b28755d51889812b19511420eb'
 
 qdrant_url = os.environ.get('QDRANT_URL')
